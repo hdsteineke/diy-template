76d079299f078cc751fe0412f6d6a6cc4433c629
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-07-29 09:35:47 -0700

    wrote getPokedex fetch function in pokedex-service.js

---
+++ src/state/services/hooks/pokedex.js
@@ -0,0 +1,4 @@
+import { useState, useEffect }from 'react';
+import { getPokedex } from '../services/pokedex-service.js';
+
+export function usePokedex(())
---
+++ src/state/services/pokedex-service.js
@@ -0,0 +1,9 @@
+const API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+const ABILITIES_URL = `${API_URL}/abilities`;
+
+export async function getPokedex(searchParams) {
+  const res = await fetch(`${API_URL}?${searchParams.toString()}`);
+  const body = await res.json();
+
+  return body;
+}
