ccc4553aed7030c6a769e2538478e9064687ccdc
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-07-29 10:19:05 -0700

    added Pokedex to Routes in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 import Layout from './Page/Layout.jsx';
 import Home from './Home/Home.jsx';
-import List from './List/List.jsx';
+import Pokedex from './Pokedex/Pokedex.jsx'; 
 import About from './About/About.jsx';
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="list" element={<List />} />
+          <Route path="pokedex" element={<Pokedex />} />
           <Route path="about" element={<About />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
