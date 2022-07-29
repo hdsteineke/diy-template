59e877f4351dc50f7a26c51f8198359fd88a7b9b
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-07-29 10:12:36 -0700

    wrote PokemonList component and Card component, Ability components for PokemonList.jsx

---
+++ src/components/Pokedex.js/PokemonList.jsx
@@ -0,0 +1,44 @@
+//import { useEffect } from 'react';
+
+// this is the pokedex component that maps through the individual 
+//card components, displaying the pokemon in a list
+
+export default function PokemonList({ pokedex }) {
+  return (
+    <ul className={styles.PokemonList}>
+      {pokedex.map((pokemon, i) => (
+        <Card
+          key={pokemon + i}
+          pokemon={pokemon}
+        />
+      ))}
+    </ul>
+  );
+}
+
+//The card components displays individual pokemon details per card
+function Card({ pokemon }) {
+  //destructuring keys from pokemon object 
+  //and assigning them to the variable 'pokemon'
+  const { url_image, pokemon: name, ability_1 } = pokemon;
+
+  return (
+    <li className={styles.Card}>
+      <img src={url_image} alt={name} />
+
+      <h2 className={styles.Header} title={name}>
+        {name}
+      </h2>
+
+      <div className={styles.Abilities}>
+        <Ability ability={ability_1} />
+      </div>
+    </li>
+  );
+}
+
+//creates an ability component that can be called inside the Card component
+// for a cleaner look and readability
+function Ability({ ability }) {
+  return ability === 'NA' ? null : <span>{ability}</span>;
+}
--- src/state/services/pokedex-service.js
+++ src/state/services/pokedex-service.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
-const ABILITIES_URL = `${API_URL}/abilities`;
+//const ABILITIES_URL = `${API_URL}/abilities`;
 
 export async function getPokedex(searchParams) {
   const res = await fetch(`${API_URL}?${searchParams.toString()}`);
