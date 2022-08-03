2d83f4dedc673d836633a872c8bf2f261ea3bb54
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-03 12:17:47 -0700

    updated Navigation.jsx to map over NavLink commponents rather than hardcoding them

--- src/components/Page/Navigation.jsx
+++ src/components/Page/Navigation.jsx
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom';
 import styles from './Navigation.css';
 
-export default function Navigation({ navigation, to }) {
+export default function Navigation({ navigation }) {
   return (
     <nav className={styles.Navigation}>
-      {/* {navigation.map(({ to, label }) => (
+      {navigation.map(({ to, label }) => (
         <NavLink key={to} to={to}>
           {label}
         </NavLink>
-      ))} */}
-      <NavLink to="/">Home</NavLink>
+      ))}
+      {/* <NavLink to="/">Home</NavLink>
       <NavLink to="list">List</NavLink>
       <NavLink to="pokedex">Pokedex</NavLink>
       <NavLink to="fuzzy-bunny">Bunnies</NavLink>
-      <NavLink to="about">About</NavLink>
+      <NavLink to="about">About</NavLink> */}
     </nav>
   );
 }
