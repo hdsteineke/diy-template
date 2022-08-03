36ff5f922f0ab37a8fdb7bc86774852027d61665
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-03 15:46:23 -0700

    wrote Family.jsx component with event handlers for editing family names

--- src/components/FuzzyBunny/Family.jsx
+++ src/components/FuzzyBunny/Family.jsx
@@ -1,5 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+import { InputControl } from '../Forms/FormControls.jsx';
+
 export default function Family({ family }) {
+  const { remove, update } = useActions();
+  const [editing, setEditing] = useState(false);
+  const ref = useRef();
+  const [name, setName] = useState(family.name);
+
+  const handleRemove = () => remove(family.id);
+
+  //allows user to begin editing a family name on double-click
+  const handleDoubleClick = () => {
+    setEditing(true);
+  };
+
+  //when editing a family name, cursor focused in this field
+  useEffect(() => {
+    if (editing) ref.current.focus();
+  }, [editing]);
+
+  //allows user to create a family's name
+  const handleChange = ({ target }) => {
+    setName(target.value);
+  };
+
+  //allows user to edit an existing family name
+  const handleEdit = async () => {
+    setEditing(false);
+    if (name === family.name) return;
+    await update({ name, id: family.id });
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.code === 'Enter') handleEdit();
+  };
+
   return (
-    <div>{family.name}</div>
+    <li>
+      {/* if editing, display input form, else display family name */}
+      {editing ? (
+        <InputControl
+          ref={ref}
+          name="name"
+          value={name}
+          onChange={handleChange}
+          onKeyUp={handleKeyUp}
+          onBlur={handleEdit}
+        />
+      ) : (
+        <h3 onDoubleClick={handleDoubleClick}>{family.name}</h3>
+      )}
+      <button onClick={handleRemove}>Delete</button>
+    </li>
   );
 }
