c3439a61a48c9a06a2b3482d437757712a6fde12
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-03 16:07:29 -0700

    basic styling for spacing out families

--- src/components/FuzzyBunny/FamilyList.jsx
+++ src/components/FuzzyBunny/FamilyList.jsx
@@ -1,5 +1,6 @@
 import { useFamilies } from '../../state/hooks/fuzzyBunny.js';
 import Family from './Family.jsx';
+import styles from './FamilyList.css';
 
 export default function FamilyList() {
   const { families } = useFamilies();
@@ -7,7 +8,7 @@ export default function FamilyList() {
   if (!families) return null;
 
   return (
-    <ul>
+    <ul className={styles.FamilyList}>
       {families.map((family) => (
         <Family key={family.id} family={family} />
       ))}
