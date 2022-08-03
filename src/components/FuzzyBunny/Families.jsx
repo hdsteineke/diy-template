3ae9dc460f9851e5403a36bcf9c4fd615a9f6b65
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-03 16:20:32 -0700

    successfully adding families to database and displaying them on Fammilies page

--- src/components/FuzzyBunny/Families.jsx
+++ src/components/FuzzyBunny/Families.jsx
@@ -1,24 +1,12 @@
-import Family from './Family';
 import FamilyList from './FamilyList';
-// import AddFamily from './AddFamily.jsx';
+import AddFamily from './AddFamily';
 
 export default function Families() {
   // const { families } = useFamilies();
   return (
     <div>
-      {/* <AddFamily /> */}
+      <AddFamily />
       <FamilyList />
-
-      {/* <ul>
-        <li>
-          <h3>the Steineke family</h3>
-          <ul>
-            <li>Fuzzylumpkins</li>
-            <li>Mojo Jojo</li>
-          </ul>
-
-        </li>
-      </ul> */}
     </div>
   );
 }
