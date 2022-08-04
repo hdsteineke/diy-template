ee8028ce61f9cee8a8a29d5050fca7d3b424bd8e
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-03 17:23:51 -0700

    updated FormControl.jsx

--- src/components/Forms/FormControls.jsx
+++ src/components/Forms/FormControls.jsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import styles from './FormControls.css';
+import { Children, cloneElement, forwardRef } from 'react';
 
 function FormControl({
   label,
@@ -20,41 +21,85 @@ function FormControl({
 }
 
 function Label({ text }) {
+  if (!text) return null;
+
   return <span className="label-text">{text}</span>;
 }
 
-export function CheckboxControl({ label, text, ...rest }) {
+//allows for easy customization of different option types (options of options!)
+function Option({ text, type, ...rest }) {
+  return (
+    <label>
+      <input type={type} {...rest} />
+      {text}
+    </label>
+  );
+}
+
+
+export function CheckboxOption(props) {
+  return <Option type="checkbox" {...props} />;
+}
+
+export function RadioOption(props) {
+  return <Option type="radio" {...props} />;
+}
+
+
+export function CheckboxControl({ label, ...rest }) {
   return (
     <div className={styles.FormControl}>
       <Label text={label} />
-      <label className={styles.CheckboxLabel}>
-        <input type="checkbox" {...rest} />
-        {text}
-      </label>
+      <CheckboxOption {...rest} />
     </div>
   );
 }
 
-export function InputControl({
+//?? maybe a way of keeping formatting consistent across options?
+export function OptionGroupControl({
   label,
-  className,
-  ...rest
+  name,
+  children,
 }) {
-  return (
-    <FormControl label={label} className={className}>
-      <input {...rest} />
-    </FormControl>
+  return(
+    <div>
+      <fieldset>
+        <Label text={label} as="legend" />
+        <div>
+          {Children.map(children, (child) =>
+            cloneElement(child, { name })
+          )}
+        </div>
+      </fieldset>
+    </div>
   );
 }
+
+//?? What is going on here with forwardRef?
+export const InputControl = forwardRef(
+  ({ label, className, value, ...rest }, ref) => {
+    return (
+      <FormControl label={label} className={className}>
+        <input ref={ref} value={value || ''} {...rest} />
+      </FormControl>
+    );
+  }
+);
+
+InputControl.displayName = 'InputControl';
+
 
 export function SelectControl({
   label,
   children,
+  value,
   ...rest
 }) {
   return (
     <FormControl label={label}>
-      <select {...rest}>{children}</select>
+      <select value={value || ''} {...rest}>
+        {children}
+      </select>
     </FormControl>
   );
 }
@@ -67,12 +112,17 @@ export function TextAreaControl({ label, ...rest }) {
   );
 }
 
-export function FormButton({ children }) {
+export function FormButton({ children, ...rest }) {
   return (
-    <button className={styles.FormButton}>
+    <button className={styles.FormButton} {...rest}>
       {children}
     </button>
   );
+}
+
+
+export function FormButtonControl(props) {
+  return <FormButton {...props} />;
 }
 
 export function Fieldset({ legend, children }) {
