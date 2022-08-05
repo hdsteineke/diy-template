7e644587cc6a04dabf266baaf0a0b18b5b629344
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-05 11:30:49 -0700

    fleshing out user-service.js for building out a user profile

--- src/state/services/user-service.js
+++ src/state/services/user-service.js
@@ -16,3 +16,62 @@ export async function signOut() {
   return await client.auth.signOut();
 }
 
+const PROFILE = 'profile';
+export async function getLocalProfile() {
+  const json = localStorage.getItem(PROFILE);
+  if (!json) return null;
+  try {
+    return JSON.parse(json);
+  } catch (err) {
+    localStorage.removeItem(PROFILE);
+  }
+}
+
+export async function saveLocalProfile(profile) {
+  localStorage.setItem(PROFILE, JSON.stringify(profile));
+}
+
+export async function getProfile() {
+  const user = getUser();
+
+  return await client
+    .from('profiles')
+    .select()
+    .eq('id', user.id)
+    .single();
+}
+
+export async function updateProfile(profile) {
+  const response = await client
+    .from('profiles')
+    .upsert(profile)
+    .eq('id', profile.id)
+    .single();
+
+  saveLocalProfile(response.data);
+  return response;
+}
+
+const BUCKET_NAME = 'avatars';
+
+export async function uploadAvatar(userId, imageFile) {
+  const imageName = `${userId}/${imageFile.name}`;
+
+  const bucket = client.storage.from(BUCKET_NAME);
+
+  const { data, error } = await bucket.upload(imageName, imageFile, {
+    cacheControl: '3600',
+
+    upsert: true,
+  });
+
+  let url = null;
+
+  if (!error) {
+    url = bucket.getPublicUrl(
+      data.Key.replace(`${BUCKET_NAME}/`, '')
+    ).publicURL;
+  }
+
+  return { url, error };
+}
