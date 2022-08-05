0514380d4131a34c1e3ca57ff080a0eb75c21b32
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-05 12:48:14 -0700

    updated userAuth.js to redirect unauthenticated users to sign in page

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,10 +1,25 @@
 import Auth from './Auth.jsx';
+import { useStatus } from '../../state/hooks/userAuth.js';
+import Profile from './Profile.jsx';
+
 
 export default function UserAuth() {
+  const { user, profile } = useStatus();
+
   return (
     <section>
       User Auth section
-      <Auth />
+      {/* checks for authorized user: if no, reroute to Auth. 
+      if yes, reroute to profile */}
+      {user ? <Profile /> : <Auth />}
+
+      {/* hardcoded preview of user data for checking authentication */}
+      <div>
+        <h2>User</h2>
+        <pre>{JSON.stringify(user, true, 2)}</pre>
+        <h2>Profile</h2>
+        <pre>{JSON.stringify(profile, true, 2)}</pre>
+      </div>
     </section>
   );
 }
