ae36a6ffe73bdf7fb24af9734d61dc2082a29f0f
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-05 13:47:42 -0700

    updated userAuth.js to include useAuth function

--- src/state/hooks/userAuth.js
+++ src/state/hooks/userAuth.js
@@ -1,14 +1,38 @@
 import { useContext } from 'react';
 import {
+  signIn as signInService,
+  signUp as signUpService,
+} from '../services/user-service.js';
+import {
   UserStateContext,
   UserActionContext
 } from '../context/UserContext.jsx';
 
-export function useAuthStatus() {
+export function useStatus() {
   const { user, profile } = useContext(UserStateContext);
 
+  return { user, profile };
+}
+
+export function useAuth() {
+  const { setUser } = useContext(UserActionContext);
+
+  const createAction = (service) => async (credentials) => {
+    const { user, error } = await service(credentials);
+
+    if (error) {
+      alert('Error!! Try again :P');
+    }
+    if (user) {
+      setUser(user);
+    }
+  };
+
+  const signIn = createAction(signInService);
+  const signUp = createAction(signUpService);
+
   return {
-    hasUser: !!user,
-    hasProfile: !!profile,
+    signIn,
+    signUp,
   };
 }
