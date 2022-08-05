58a386a0ccde46855e6dda0b4b572d6471edb4fb
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-08-05 12:04:27 -0700

    wrote formData file in hooks folder

---
+++ src/state/hooks/formData.js
@@ -0,0 +1,21 @@
+import { getValue } from '@testing-library/user-event/dist/types/utils';
+import { useState } from 'react';
+
+export function useForm() {
+  const [data, setData] = useState({});
+
+  const handleChange = ({ target }) => {
+    setData({
+      ...data,
+      [target.name]: getValue(target),
+    });
+  };
+
+  return [data, handleChange];
+}
+
+//looks like a way for managing what the form type is and how it is handled
+function getValue({ value, type, checked }) {
+  if (type === 'checkbox') return checked;
+  return value;
+}
